e4df81922e6b8250b8cd75aac6481bbe7360a971
josue <jcarmona@emergya.com>
2017-08-25 20:19:40 +0200

    change env

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Post
 
+from .forms import PostForm
+from .models import Post
 # Create your views here.
+
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')	
 	return render(request, 'blog/post_list.html', {'posts': posts})
@@ -12,5 +14,5 @@ def post_detail(request, pk):
 	return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-        form = PostForm()
-        return render(request, 'blog/post_edit.html', {'form': form})
+	form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
--- josue/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/web/Django-Learning/josue/bin/python3.4
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
