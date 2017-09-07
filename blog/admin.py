96dfdbbad84df0db2ec4e557b026402d1dbaf1ed
josue <jcarmona@emergya.com>
2017-09-07 11:17:44 +0200

    add list on admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,13 @@
 # Register your models here.
 
 from django.contrib import admin
-from .models import Post
+from . models import Post
 
 admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('author', 'title', 'email', 'created_date')
+
+admin.site.unregister(Post)
+admin.site.register(Post, PostAdmin)
+
