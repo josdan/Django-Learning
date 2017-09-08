0218fdf20eaaba0a15f1d7fbbbd843433c28be0e
josue <jcarmona@emergya.com>
2017-09-08 09:20:46 +0200

    finish login and comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 # Register your models here.
 
 from django.contrib import admin
-from . models import Post
+from . models import Post, Comment
 
 admin.site.register(Post)
 
@@ -9,5 +9,6 @@ class PostAdmin(admin.ModelAdmin):
 	list_display = ('author', 'title', 'email', 'created_date')
 
 admin.site.unregister(Post)
+admin.site.register(Comment)
 admin.site.register(Post, PostAdmin)
 
